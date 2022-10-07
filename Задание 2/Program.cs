c5028ea035b4124a86fe22ff5a6d21b731dcb990
Marina <114664502+marina2523@users.noreply.github.com>
2022-10-07 20:28:30 +0300

    Add files via upload

---
+++ Задание 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Exercise2
+{
+    static void Main()
+    {
+        const double Pi = 3.14;
+
+        Console.WriteLine("Введите радиус окружности: ");
+        try
+        {
+            double R = Convert.ToDouble(Console.ReadLine());
+
+            double L = R*(2 * Pi);
+            Console.WriteLine(String.Format("Длина: {1} \n Площадь: {1}", R, Pi * R * R));
+
+        }
+        catch (FormatException e)
+        {
+            Console.Error.WriteLine("Радиус окружности введен неверно! ");
+        }
+    }
+}
