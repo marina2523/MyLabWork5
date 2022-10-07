7e3c5c0e1492c2c08cc659ec3c660b36e8882633
Marina <114664502+marina2523@users.noreply.github.com>
2022-10-07 20:27:40 +0300

    Add files via upload

---
+++ Задание 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("1 13 49");// вывд данных на экран,через один пробел.
+    }
+}
+
