73a02e034c322cf6674574157a0e182909b91c83
Marina <114664502+marina2523@users.noreply.github.com>
2022-10-07 20:30:46 +0300

    Add files via upload

---
+++ задание 3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Exercise3
+{
+    static void Main()
+    {
+        Console.WriteLine("Прошло дня:");
+        int i = int.Parse(Console.ReadLine());
+        Console.Write("Полных недель: ");
+        Console.WriteLine(i / 7);
+    }
+}
